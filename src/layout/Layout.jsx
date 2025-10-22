06866892a1906254aef255080c1d8b5aca66cbea
Md. Shoyaif Rahman <sohan0020p@gmail.com>
2025-10-22 21:47:53 +0600

    firebase

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Footer } from '../components/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router'
 import { Navbar } from '../components/Navbar'
 
 export const Layout = () => {
